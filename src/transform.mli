cb767432e2c76b5c1a65d93275eb032f6d85f4d2
Jordan Scales <scalesjordan@gmail.com>
2019-01-26 16:47:21 -0500

    expression list -> program

--- src/transform.mli
+++ src/transform.mli
@@ -2,10 +2,11 @@ open Lang
 
 val swap_variable : string -> string -> expression -> expression
 exception TraversalError
-val flatten : expression list -> expression list
-val num_exprs : expression list -> int
-val traverse : expression list -> int -> expression
+val flatten : program -> expression list
+val num_exprs : program -> int
+val traverse : program -> int -> expression
 type replace_opts = { expr : expression; desired : expression; pos : int; }
 val replace : replace_opts -> expression
 type abstract_opts = { expr : expression; name : string; pos : int; }
 val abstract : abstract_opts -> expression
+
